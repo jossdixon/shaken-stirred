4c84a3e66fdd6e01789e92f041b8b2a1a5297164
Joss Dixon <josscdixon@gmail.com>
2022-01-06 11:01:49 +0000

    updating models

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,2 +1,7 @@
 class Cocktail < ApplicationRecord
+  validates :name, uniqueness: true
+  validates :name, presence: true
+
+  has_many :measures, dependent: destroy
+  has_many :ingredients, through: :measures
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,5 @@
 class Ingredient < ApplicationRecord
+  validates :name, uniqueness: true
+
+  has_many :measures
 end

2a2612bd09ab498a45e3e2343ec26156431da73b
Joss Dixon <josscdixon@gmail.com>
2022-01-06 16:17:08 +0000

    routing controllers and views completed

--- app/controllers/measure_controller.rb
+++ app/controllers/measure_controller.rb
@@ -1,2 +1,14 @@
-class MeasureController < ApplicationController
+class MeasuresController < ApplicationController
+  def new
+    @measure = Measure.new
+  end
+
+  def create
+    @measure = Measure.new(measure.params)
+    if @measure.save
+      redirect_to measure_path(@measure)
+    else
+      render :new
+    end
+  end
 end

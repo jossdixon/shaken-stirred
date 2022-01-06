249bcd0db7d210e8f0e58ecf6a626e71dbb17b3e
Joss Dixon <josscdixon@gmail.com>
2022-01-06 12:52:55 +0000

    seeding completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_list = URI.open(url).read
+ingredients = JSON.parse(ingredients_list)
+
+puts 'Initializing seeding engines...'
+ingredients['drinks'].each do |thing|
+  Ingredient.create(name: thing["strIngredient1"])
+end
+puts 'Seeding completed.'

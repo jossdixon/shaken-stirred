8fb34428b79e71f2a9cbf19aeb8c36e698bf1ec2
Joss Dixon <josscdixon@gmail.com>
2022-01-20 14:27:11 +0000

    controller updated

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,18 @@
 class CommentsController < ApplicationController
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @comment = Comment.new(comment_params)
+    @comment.cocktail = @cocktail
+    if @comment.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'cocktails/show'
+    end
+  end
 
+  private
+
+  def cocktail_params
+    params.require(:comment).permit(:content)
   end
 end

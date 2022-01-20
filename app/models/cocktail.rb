55614132f8ec3b32e055aafbaa6f444a8c4295c8
Joss Dixon <josscdixon@gmail.com>
2022-01-20 13:28:14 +0000

    models updated

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,5 +4,6 @@ class Cocktail < ApplicationRecord
 
   has_many :measures, dependent: :destroy
   has_many :ingredients, through: :measures
+  has_many :comments, dependent: :destroy
   has_one_attached :photo
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :cocktail
+  validates :content, length: { minimum: 20 }
 end
